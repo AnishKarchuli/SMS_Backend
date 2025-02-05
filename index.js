04cafc49d9cd6b5c0c99c5c96f32fd999106eced
AnishKarchuli <aksk099@hotmail.com>
2025-02-05 22:36:17 +0530

    all files added

---
+++ index.js
@@ -0,0 +1,116 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+require("dotenv").config();
+
+app.use(express.json());
+
+const { initializeDatabase } = require("./db/db.connect");
+// const fs = require("fs");
+const Student = require("./models/student.model");
+initializeDatabase();
+
+// const jsonData = fs.readFileSync("students.json", "utf-8");
+// const studentsData = JSON.parse(jsonData);
+
+/*
+function seedData() {
+  try {
+    for (const studentData of studentsData) {
+      const newStudent = new Student({
+        name: studentData.name,
+        age: studentData.age,
+        gender: studentData.gender,
+        marks: studentData.marks,
+        attendance: studentData.attendance,
+        grade: studentData.grade,
+      });
+      newStudent.save();
+    }
+  } catch (error) {
+    console.log("Error seeding the data", error);
+  }
+}
+*/
+
+// seedData();
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+app.get("/students", async (req, res) => {
+  try {
+    const students = await Student.find();
+    res.json(students);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/students", async (req, res) => {
+  const { name, age, grade } = req.body;
+
+  try {
+    const student = new Student({ name, age, grade });
+    await student.save();
+    res.status(201).json(student);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.put("/students/:id", async (req, res) => {
+  const studentId = req.params.id;
+  const updatedStudentData = req.body;
+
+  try {
+    const updatedStudent = await Student.findByIdAndUpdate(
+      studentId,
+      updatedStudentData,
+      { new: true }
+    );
+
+    if (!updatedStudent) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+
+    res.status(200).json(updatedStudent);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+app.delete("/students/:id", async (req, res) => {
+  const studentId = req.params.id;
+
+  try {
+    const deletedStudent = await Student.findByIdAndRemove(studentId);
+
+    if (!deletedStudent) {
+      return res.status(404).json({ error: "Student not found" });
+    }
+
+    res.status(200).json({
+      message: "Student deleted successfully",
+      student: deletedStudent,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
